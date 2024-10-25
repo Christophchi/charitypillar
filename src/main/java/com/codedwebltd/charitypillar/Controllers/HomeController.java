1ac6be439b3635eca1ddb24d45804c3ccee45bec
bcoded200 <dakingeorge58@gmail.com>
2024-10-25 09:09:08 -0400

    testing the railway post request and mysql plugins.

--- src/main/java/com/codedwebltd/charitypillar/Controllers/HomeController.java
+++ src/main/java/com/codedwebltd/charitypillar/Controllers/HomeController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,5 +46,10 @@ public class HomeController {
         model.addAttribute("totalTransactions", totalTransactions);
 
         return "home/index"; // Ensure this points to the correct file
+    }
+    @ResponseBody
+    @PostMapping("/uploadPost")
+    public String[] textPostRequest(){
+      return new String[]{"post-updated", "second-post", "third-post"};
     }
 }
